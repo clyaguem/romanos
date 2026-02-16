afb61cb8330cea50338e225f6f1ffd6cf4d0dada
Carolina <clyaguem@gmail.com>
2026-02-16 21:04:06 +0100

    segunda versión con cambio de lógica

--- main.py
+++ main.py
@@ -19,14 +19,13 @@ I -> 1
 
 """
 Creamos unos diccionarios para poder tener la equivalencia en números de los romanos
-Lo dividimos en unidades, decenas, centenas y millares para poder abarcar todos los números que queremos
 """
-diccionario = {1000:"M",500:"D",100:"C",50:"L",10:"X",5:"V",1:"I"}
-
-unidades = {1:"I",2:"II",3:"III",4:"IV",5:"V",6:"VI",7:"VII",8:"VIII",9:"IX"}
-decenas = {10:"X",20:"XX",30:"XXX",40:"XL",50:"L",60:"LX",70:"LXX",80:"LXXX",90:"XC"}
-centenas = {100:"C",200:"CC",300:"CCC",400:"CD",500:"D",600:"DC",700:"DCC",800:"DCCC",900:"CM"}
-millares = {1000:"M",2000:"MM",3000:"MMM"}
+diccionario_entero_a_romano = {
+    1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX",
+    10: "X", 20: "XX", 30: "XXX", 40: "XL", 50: "L", 60: "LX", 70: "LXX", 80: "LXXX", 90: "XC",
+    100: "C", 200: "CC", 300: "CCC", 400: "CD", 500: "D", 600: "DC", 700: "DCC", 800: "DCCC", 900: "CM",
+    1000: "M", 2000: "MM", 3000: "MMM"
+}
 
 """
 Crearemos un error
@@ -38,32 +37,28 @@ class RomanNumberError(Exception):
 # a) 1994 -> MCMXCIV
 def entero_a_romano(numero):
     numero = str(numero) #Transformamos en cadena el valor 1994 para poder descomponerlo
+    if len(numero) == 3: #Si la longitud de número es igual a 3
+        numero = "0" + numero #Añade un 0 delante del número
+    elif len(numero) == 2: #Si la longitud de número es igual a 2
+        numero = "00" + numero #Añade dos 0 delante del número
+    elif len(numero) == 1: #Si la longitud de número es igual a 1
+        numero = "000" + numero #Añade tres 0 delante del número
+
     numero_list = list(numero) #Guardardamos en una lista la cadena resultante del paso anterior ["1","9","9","4"]
-    valor_romano = " " #Guarda los resultados de las condiciones
-    for i in range(0,len(numero_list)): #Recorremos con el índice la variable "número" para poder saber si son millares, centenas, decenas o unidades
-        if i == 0: #Si el indice está en la posición 0 debería ser millar
-            numero_list[i] = int(numero_list[i]) * 1000 #Si se cumple la condición anterior, el valor de la posición 0 se transforma en integro y se multiplica por 1000
-            valor_romano += millares.get(numero_list[i])
-        if i == 1: #Si el indice está en la posición 1 debería ser millar
-            numero_list[i] = int(numero_list[i]) * 100 #Si se cumple la condición anterior, el valor de la posición 1 se transforma en integro y se multiplica por 100
-            valor_romano += centenas.get(numero_list[i])
-        if i == 2: #Si el indice está en la posición 2 debería ser millar
-            numero_list[i] = int(numero_list[i]) * 10 #Si se cumple la condición anterior, el valor de la posición 2 se transforma en integro y se multiplica por 10
-            valor_romano += decenas.get(numero_list[i])
-        if i == 3:
-            numero_list[i] = int(numero_list[i])
-            valor_romano += unidades.get(numero_list[i])
-        
-    print(valor_romano)
+    valor_romano = " " #Guarda los resultados obtenidos con cada vuelta del while
+    contador = 0
+    valor_num = 1000
 
+    while contador < len(numero_list): #Mientras contador sea menor que la longitud de la lista de números
+        numero_list[contador] = int(numero_list[contador])*valor_num #El valor se convierte a integro y se multiplica por la variable "valor_num"(1000)
+        valor_romano += diccionario_entero_a_romano.get(numero_list[contador],"") #Busca el valor en el diccionario unificado, si no encuentra el valor añade una cadena vacía
+        contador += 1 #El contador se mueve una posición
+        valor_num /= 10 #En cada vuelta, la variable valor_num se divide entre 10 pasando de millares a centenas, decenas...
 
     
-    
-    
-    
-    return "MCMXCIV"
+    return valor_romano
 
-entero_a_romano(1994)
+print(entero_a_romano(3))
 
 #1994
 #["1","9","9","4"]
